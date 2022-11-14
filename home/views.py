81ce9bfbe6f06a2b8d2624d44ca5f685c6f74909
Motrapatoni <moiseserp@gmail.com>
2022-11-14 05:52:11 -0300

    estilos

--- home/views.py
+++ home/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Template, Context, loader
-#from models import *
 
 # Create your views here.
 def index(request):
--- items/urls.py
+++ items/urls.py
@@ -5,6 +5,8 @@ from items import views
 urlpatterns = [
     path('', views.Items.as_view(), name='items'),
     path('create-items/', views.CreateItem.as_view(), name='create-items'),
-    path('read-item/<int:pkj>/',views.ReadItem.as_view() ,name='read-item'),
+    path('read/<int:pk>', views.ReadItem.as_view(), name='read'),
+    path('edit/<int:pk>', views.EditItem.as_view(), name='edit-item'),
+    path('delete/<int:pk>', views.DeleteItem.as_view(), name='delete-item'),
     path('contact/', views.contact, name='contact'),
 ]
